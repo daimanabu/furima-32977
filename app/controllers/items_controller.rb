c336e9cd0b4cc806150da91bbc55f17ca0536c5e
daimanabu <daimanabu777@gmail.com>
2020-12-24 12:06:30 +0900

    バリデーションを記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,16 @@
 class ItemsController < ApplicationController
+
+  before_action :move_to_index, except: [:index, :show]
+  
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+
+  end
+
 end
